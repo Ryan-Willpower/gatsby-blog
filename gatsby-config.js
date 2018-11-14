c1dd1e9ba1ce9b87860b46bd84506ea718ea6b37
Ryan-Willpower <surasak.c.work@gmail.com>
2018-11-14 16:34:40 +0700

    [update] add plugins

---
+++ gatsby-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/static/images/`,
+      },
+    },
+  ]
+}
